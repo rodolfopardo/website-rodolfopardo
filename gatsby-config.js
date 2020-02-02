8d357319d007752a518c81208f8aa66f9595016e
Rodolfo Pardo <rodypardo@gmail.com>
2020-02-02 06:19:44 -0300

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-sitemap`,
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-datocms`,
@@ -23,6 +22,40 @@ module.exports = {
         trackingId: "UA-157539657-1",
         head: true
       },
-    },    
-  ],
+    },
+   {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+      output: `/some-other-sitemap.xml`,
+      // Exclude specific pages or groups of pages using glob parameters
+      // See: https://github.com/isaacs/minimatch
+      // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+      exclude: [`/category/*`, `/path/to/page`],
+      query: `
+        {
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+
+          allSitePage {
+            edges {
+              node {
+                path
+              }
+            }
+          }
+      }`,
+      serialize: ({ site, allSitePage }) =>
+        allSitePage.edges.map(edge => {
+          return {
+            url: site.siteMetadata.siteUrl + edge.node.path,
+            changefreq: `daily`,
+            priority: 0.7,
+          }
+        })
+    }
+  } 
+ ],
 }
